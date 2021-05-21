666bb981fdb91207e2bad30e1fc73eb136b05211
elena-3 <78790971+elena-3@users.noreply.github.com>
2021-05-21 19:36:01 +0200

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,23 @@
+package com.exmple.helloworld;
+
+import java.awt.desktop.SystemEventListener;
+
+public class Helloworld {
+    public static void main(String[] args) {
+        /*
+
+
+
+         */
+        //commento
+        int i = 10;
+        if (i > 10) {
+            System.out.println("ciao");
+        }
+
+        for(int k=0; k<5; ++k)
+        {
+            System.out.println("k");
+        }
+    }
+}
